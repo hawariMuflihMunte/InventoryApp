c30923927569314174abfbd1012568adfaa4fd94
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-10-29 01:26:32 +0700

    Update: annotation to data class Item - @Entity - @PrimaryKey

--- app/src/main/java/com/example/inventory/data/Item.kt
+++ app/src/main/java/com/example/inventory/data/Item.kt
@@ -17,6 +17,7 @@
 package com.example.inventory.data
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 
 /**
@@ -24,6 +25,7 @@ import androidx.room.Entity
  */
 @Entity(tableName = "items")
 data class Item(
+    @PrimaryKey
     val id: Int = 0,
     val name: String,
     val price: Double,
